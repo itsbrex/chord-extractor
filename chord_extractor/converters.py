ff597dffcf9d5a0b7fafaedf5b2109d9c0eb478f
Oliver Holloway <oliverholloway@yahoo.co.uk>
2025-08-14 17:41:40 +0200

    Reinstate midi error handling logic, and fix numpy version in requirements

--- chord_extractor/converters.py
+++ chord_extractor/converters.py
@@ -20,26 +20,31 @@
 import os
 import subprocess
 import logging
+from typing import Optional
 
 _log = logging.getLogger(__name__)
+_ERROR_SIGNAL_TEXT = "Not a MIDI file"
 
 
-def midi_to_wav(midi_path: str, wav_to_dir: str):
+def midi_to_wav(midi_path: str, wav_to_dir: str) -> Optional[str]:
     """
     Convert midi at given path to wav file and save in specified output directory. This is done using
-    Timidity (http://timidity.sourceforge.net/). If an invalid midi file is passed, note that no exception is raised
-    and an output file containing invalid wav data.
+    Timidity (http://timidity.sourceforge.net/). Note that no exception is raised when an invalid midi_path is given
+    (None is returned).
 
     :param midi_path: Path to input midi file
     :param wav_to_dir: Path to the output directory
-    :return: Path to the new wav file
+    :return: Path to the new wav file. None if an invalid midi file is passed.
     """
     base = os.path.basename(midi_path)
     file_name = os.path.splitext(base)[0]
     wav_file = os.path.join(wav_to_dir, file_name)
     if not os.path.isfile(wav_file):
         _log.info('Running timidity on {} to create {}'.format(midi_path, wav_file))
-        subprocess.run(['timidity', midi_path, '-Ow', '-o', wav_file])
+        result = subprocess.run(['timidity', midi_path, '-Ow', '-o', wav_file], stdout=subprocess.PIPE, text=True, errors="replace")
+        if _ERROR_SIGNAL_TEXT in result.stdout:
+            _log.error('Invalid midi file at {}'.format(midi_path))
+            return None
     else:
         _log.info('Returning already existing temporary wav file {}'.format(wav_file))
     return wav_file
